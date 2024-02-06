050705bb2bfc999d2982a929148cd4e36b31f8f9
acupoflie <ishyeen@gmail.com>
2024-02-06 07:04:25 +0400

    solved cors problem, nexttttt

--- client/public/scripts/authLogin.js
+++ client/public/scripts/authLogin.js
@@ -6,19 +6,35 @@
 
 // import setCookie from './jsCookie'
 
-
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    fetch('http://localhost:3355/auth/login', {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:4455/auth/login', {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
-        body: {
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value
-        }
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("hello mother fucker")
+        const jwtToken = data.token;
+        console.log('JWT token has received', jwtToken);
+
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 1000 * 60 * 60 * 24);
+
+        const cookieString = `jwt=${encodeURIComponent(jwtToken)};max-age=${expirationDate.toUTCString()};path=/`;
+        document.cookie = cookieString;
+
+        // window.location.href = '/index.html'
     })
 
 })
--- server/app.js
+++ server/app.js
@@ -17,19 +17,13 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-app.use(cors({origin: 'http://localhost:7777', credentials: true}));
-
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-//     next();
-// })
-// app.use(cors())
+app.use(cors({origin: 'http://localhost:3001'}))
 
 // app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // Replace with your client's origin
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); 
+//     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type');
 //     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
 //     next();
 //   });
 
