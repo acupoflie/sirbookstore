8ad08616a159728194207f00ff267d79ec6adfda
acupoflie <ishyeen@gmail.com>
2024-02-06 06:59:24 +0400

    solved cors problem

--- client/public/scripts/authLogin.js
+++ client/public/scripts/authLogin.js
@@ -2,30 +2,35 @@
 // import Cookies from '../../../node_modules/js-cookie';
 // import * as Cookies from 'js-cookie';
 
-import Cookies from '../../server/Cookies';
-
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    fetch('http://localhost:3355/auth/login', {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:4455/auth/login', {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
-        body: {
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value
-        }
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
     })
     .then(res => res.json())
     .then(data => {
+        console.log("hello mother fucker")
         const jwtToken = data.token;
-
         console.log('JWT token has received', jwtToken);
 
-        Cookies.set('jwtTokens', jwtToken, {expires: 1});
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 1000 * 60 * 60 * 24);
 
-        windows.location.href = '/index.html'
+        const cookieString = `jwt=${encodeURIComponent(jwtToken)};max-age=${expirationDate.toUTCString()};path=/`;
+        document.cookie = cookieString;
+
+        // window.location.href = '/index.html'
     })
 
 })
