768db24917ad3a803849ffec045e06e30511bab9
Sir <daupa@Sirs-MacBook-Pro.local>
2024-02-10 23:51:43 +0400

    implementing navbar module

--- client/public/scripts/accountNavBar.js
+++ client/public/scripts/accountNavBar.js
@@ -1,14 +1,29 @@
 
 
 (function() {
-    fetch('navBarAccount.html')
-    .then((res) => res.text())
-    .then(html => {
 
-        const parser = new DOMParser();
-        const module = parser.parseFromString(html, 'text/html').body.firstChild;
+    if(!(Cookies.get('jwt'))) {
+        fetch('navBarAccount.html')
+        .then((res) => res.text())
+        .then(html => {
+    
+            const parser = new DOMParser();
+            const module = parser.parseFromString(html, 'text/html').body.firstChild;
+    
+            const container = document.getElementById('navbarList');
+            container.insertBefore(module, container.firstChild)
+        })
+    } else if (Cookies.get('jwt')) {
+        fetch('navBarAccount.html')
+        .then((res) => res.text())
+        .then(html => {
+    
+            const parser = new DOMParser();
+            const module = parser.parseFromString(html, 'text/html').body.children[1];
+    
+            const container = document.getElementById('navbarList');
+            container.insertBefore(module, container.firstChild)
+        })
+    }
 
-        const container = document.getElementById('navbarList');
-        container.insertBefore(module, container.firstChild)
-    })
 })()
